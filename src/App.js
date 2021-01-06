05a6fd27dba98ee9e10dbaff8e63f380e575b1d4
hojinh <hojinh@gmail.com>
2021-01-06 19:46:39 +0900

    add script page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import './App.css';
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import Home from './components/home';
 import Profile from './components/profile';
+import Script from './components/script';
+
+const navStyle = {
+  display : "flex",
+  justifyContent: "space-around",
+  padding : "1em",
+}
 
 function App() {
   return (
    <BrowserRouter>
-      <nav>
+      <nav style={navStyle}>
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
+        <Link to="/script">Script</Link>
       </nav>
       <Switch>
           {/*추천하지 않는다 */}
@@ -18,6 +26,9 @@ function App() {
           {/*자식요소로 넘겨주고 자식요소에서 useHistory push를 사용하자*/}
           <Route path="/profile">
             <Profile />
+          </Route>
+          <Route path="/script">
+            <Script />
           </Route>
         </Switch>
    </BrowserRouter>
