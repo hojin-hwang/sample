6fb54a40eac617f0b7352f2f3142db6de39d71ce
hojinh <hojinh@gmail.com>
2021-01-06 19:00:03 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import Home from './components/home';
+import Profile from './components/profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/profile">Profile</Link>
+      </nav>
+      <Switch>
+          {/*추천하지 않는다 */}
+          <Route path={['/','/home']} exact component = {Home}> 
+          </Route>
+
+          {/*자식요소로 넘겨주고 자식요소에서 useHistory push를 사용하자*/}
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+   </BrowserRouter>
   );
 }
 
